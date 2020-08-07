8f4a08268471e7f495a3b66dd7695d8f9e77a3ad
Wrg1t <Wrg1t@users.noreply.github.com>
2020-08-07 15:00:30 +0800

    master

--- eqBOT-COOLQ.py
+++ eqBOT-COOLQ.py
@@ -5,20 +5,8 @@ import time
 import traceback
 
 def getHTML(url):
-    headers = {
-        'authority': 'weibo.cn',
-        'cache-control': 'max-age=0',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36 Edg/84.0.522.49',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'none',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-        'cookie': 'SCF=AjnntoaCX-Y_eEN_kE9nWAWVgbgDY_jD2Bi5-a9OjlR43Qw-1Q_DR8oveHBhH-6g32THOG3nXX2-NkKzGEhZHMw.; SUB=_2A25yEE84DeRhGeNP41QV8S_EzTuIHXVR-1FwrDV6PUJbktANLU_hkW1NTk9FXg679p-2GqXQ3xwZ5cUNVVzADcm8; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5E0SIN25KcaEzfuONp--9J5JpX5K-hUgL.Fo-p1hqXeK2RSoM2dJLoIp7LxKML1KBLBKnLxKqL1hnLBoMfeKncSh2p1hqN; SUHB=04WuvNgdU_Yi-0; ALF=1597754473; _T_WM=38edaedf50b31a8743ff5498485c92ab',
-    }
     try:
+        headers = {''}  # 自行获取header，因涉及cookies故删去
         r = requests.get(url, headers=headers)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
--- eqBOT.py
+++ eqBOT.py
@@ -8,20 +8,8 @@ import win32con
 import win32clipboard as w
 
 def getHTML(url):
-    headers = {
-        'authority': 'weibo.cn',
-        'cache-control': 'max-age=0',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36 Edg/84.0.522.40',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'none',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-        'cookie': '_T_WM=2e729b5ceb497ba787ac8d7a16384b3d; SCF=AjnntoaCX-Y_eEN_kE9nWAWVgbgDY_jD2Bi5-a9OjlR43Qw-1Q_DR8oveHBhH-6g32THOG3nXX2-NkKzGEhZHMw.; SUB=_2A25yEE84DeRhGeNP41QV8S_EzTuIHXVR-1FwrDV6PUJbktANLU_hkW1NTk9FXg679p-2GqXQ3xwZ5cUNVVzADcm8; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5E0SIN25KcaEzfuONp--9J5JpX5K-hUgL.Fo-p1hqXeK2RSoM2dJLoIp7LxKML1KBLBKnLxKqL1hnLBoMfeKncSh2p1hqN; SUHB=04WuvNgdU_Yi-0; ALF=1597754473',
-    }
     try:
+        headers = {''}  # 自行获取header，因涉及cookies故删去
         r = requests.get(url, headers=headers)
         r.raise_for_status()
         r.encoding = ('utf-8')
@@ -50,7 +38,7 @@ def weiboMain(dic):
 
 
 def getText():
-    """获取剪贴板文本"""
+    #获取剪贴板文本
     w.OpenClipboard()
     d = w.GetClipboardData(win32con.CF_UNICODETEXT)
     w.CloseClipboard()
@@ -58,7 +46,7 @@ def getText():
 
 
 def setText(aString):
-    """设置剪贴板文本"""
+    #设置剪贴板文本
     w.OpenClipboard()
     w.EmptyClipboard()
     w.SetClipboardData(win32con.CF_UNICODETEXT, aString)
